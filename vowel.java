dc1df1a1e4683d5b1068c2e9ab27c182aecac2d4
sumanth280 <114460414+sumanth280@users.noreply.github.com>
2023-04-04 20:48:15 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+class vowel
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the charecter");
+        char c=sc.next().charAt(0);
+        switch(c)
+        {
+            case 'a':
+            System.out.println("Vowel");
+            break;
+            case 'e':
+            System.out.println("Vowel");
+            break;
+            case 'i':
+            System.out.println("Vowel");
+            break;
+            case 'o':
+            System.out.println("Vowel");
+            break;
+            case 'u':
+            System.out.println("Vowel");
+            break;
+            case 'A':
+            System.out.println("Vowel");
+            break;
+            case 'E':
+            System.out.println("Vowel");
+            break;
+            case 'I':
+            System.out.println("Vowel");
+            break;
+            case 'O':
+            System.out.println("Vowel");
+            break;
+            case 'U':
+            System.out.println("Vowel");
+            break;
+            default:
+            System.out.println("Not a vowel");
+        }
+    }
+}
